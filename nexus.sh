bc182b90f1403df0126f700e7f29937e6c0ac2bb
SaiTejaKanneboina <saiteja631999@gmail.com>
2025-11-22 12:11:28 +0530

    Create nexus.sh

---
+++ nexus.sh
@@ -0,0 +1,9 @@
+yum install java-17-amazon-corretto -y
+cd /opt
+wget https://download.sonatype.com/nexus/3/nexus-3.86.2-01-linux-x86_64.tar.gz
+tar -zxvf nexus-3.86.2-01-linux-x86_64.tar.gz
+useradd devops
+chown -R devops:devops nexus-3.86.2-01 sonatype-work/
+cd /opt/nexus-3.86.2-01/bin/
+./nexus start
+./nexus status
