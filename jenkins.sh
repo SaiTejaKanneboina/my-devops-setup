2d2398e3157ec068045806641f96bb31b6772e45
SaiTejaKanneboina <saiteja631999@gmail.com>
2025-11-22 11:34:37 +0530

    Create jenkins.sh

---
+++ jenkins.sh
@@ -0,0 +1,11 @@
+sudo -i
+hostnamectl set-hostname "JENKINS"
+sudo -i 
+yum install java-17-amazon-corretto -y
+java -version
+sudo wget -O /etc/yum.repos.d/jenkins.repo     https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+yum install jenkins -y
+systemctl status jenkins
+systemctl start jenkins
+systemctl status jenkins
